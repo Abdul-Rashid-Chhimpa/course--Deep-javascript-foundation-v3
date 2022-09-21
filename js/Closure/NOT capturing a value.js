4489bdc21b1c4373af827d77a5ca3081d78b2cb4
Abdul Rashid Chhimpa <110084078+Rashid789@users.noreply.github.com>
2022-09-21 21:09:47 +0530

    Update NOT capturing a value.js

--- js/Closure/NOT capturing a value.js
+++ js/Closure/NOT capturing a value.js
@@ -4,3 +4,12 @@ var myTeacher = function() {
 };
 teacher = "rashid";
 myTeacher();
+
+// Modules
+var workshop = {
+    teacher: "kyle",
+    ask(question) {
+        console.log(this.teacher, question);
+    },
+};
+workshop.ask("Is this a module?");
