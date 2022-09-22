33f06d09c7ee5aa8bb5b408f5d58ed755f6fbabf
Abdul Rashid Chhimpa <110084078+Rashid789@users.noreply.github.com>
2022-09-22 22:19:22 +0530

    Add files via upload

---
+++ js/Objects(oriented)/this.js
@@ -0,0 +1,25 @@
+/* a function's this reference the exucution context for call,
+ determined entirly by how the function was called.*/
+var teacher = "kyle";
+
+function ask(question) {
+    console.log(teacher, question);
+}
+
+function otherclass() {
+    var teacher = "suzy";
+    ask("why");
+}
+otherclass();
+
+function hey(question) {
+    console.log(this.teacher, question);
+}
+
+function otherclass() {
+    var myContext = {
+        teacher: "abdul"
+    }
+    ask.call(myContext, "hello!");
+}
+otherclass();
